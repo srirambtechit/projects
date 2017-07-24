0d52d92e75f175d7f0c02dc635e367ac889a9d8d
srirambtechit <srirambtecit@gmail.com>
2017-07-24 07:18:45 +0530

    Threading code

--- ProjectLambda/src/main/java/com/msrm/jdk8/streams/Chapter2Ex1.java
+++ ProjectLambda/src/main/java/com/msrm/jdk8/streams/Chapter2Ex1.java
@@ -14,13 +14,68 @@ public class Chapter2Ex1 {
                 StandardCharsets.UTF_8); // Read file into string
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // Count all the long words in the list
-        int count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
+        // Run parallel code
+        int processors = Runtime.getRuntime().availableProcessors();
+        int incr = words.size() / processors;
+        int len = words.size();
+
+        System.out.println("Size : " + words.size());
+        System.out.println("Processors : " + processors);
+
+        WordCounter[] r = new WordCounter[processors];
+        for (int i = 0; i < processors; i++) {
+            int end = incr * (i + 1) - 1;
+            r[i] = new WordCounter(i * incr, len - end < incr ? len - 1 : end, words);
+            new Thread(r[i]).start();
         }
 
-        System.out.println("Count : " + count);
+        int summer = 0;
+        for (int i = 0; i < processors; i++) {
+            synchronized (r[i]) {
+                try {
+                    System.out.println("Main --> is awaiting for data");
+                    r[i].wait();
+                    summer += r[i].count();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Main --> Count is " + summer);
+            }
+        }
+    }
+
+    private static class WordCounter implements Runnable {
+
+        private int start;
+        private int end;
+        private List<String> words;
+        private Long count = 0L;
+
+        public WordCounter(int start, int end, List<String> words) {
+            this.start = start;
+            this.end = end;
+            this.words = words;
+        }
+
+        public long count() {
+            return count;
+        }
+
+        @Override
+        public void run() {
+            long c = 0;
+            System.out.printf("WordCounter --> Processing from %d till %d%n", start, end);
+            for (int i = start; i <= end; i++) {
+                if (words.get(i).length() > 12) c++;
+            }
+            System.out.printf("WordCounter --> %s's count is %d%n",
+                    Thread.currentThread().getName(), c);
+            synchronized (this) {
+                count = c;
+                notifyAll();
+            }
+        }
 
     }
 
